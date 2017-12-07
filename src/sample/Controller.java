d48bdba7b7b5b044a98b4c6f5605cf96d913b4e2
gurgen <gourgenmovsissian@gmail.com>
2017-12-07 17:05:12 +0400

    medplay

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,18 @@
 package sample;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
-import java.awt.*;
+
 import java.io.File;
 
 public class Controller {
@@ -16,23 +23,69 @@ public class Controller {
     Media me;
     @FXML
     MediaPlayer mp;
+    @FXML
+    Slider volume;
+//    @FXML
+//    Button open2;
 
+    private String filepath;
+    @FXML
+    public void open2(){
 
-    public void initialize(){
-        String path=new File("src/media/video.mp4").getAbsolutePath();
-            me=new Media(new File(path).toURI().toString());
-            mp=new MediaPlayer(me);
+        FileChooser fileChooser=new FileChooser();
+        FileChooser.ExtensionFilter filter=new FileChooser.ExtensionFilter("Select a file","*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+        File file=fileChooser.showOpenDialog(null);
+        filepath=file.toURI().toString();
+        if (filepath!=null){
+            me = new Media(filepath);
+            mp = new MediaPlayer(me);
             mv.setMediaPlayer(mp);
-//            mp.setAutoPlay(false);
+            mp.play();
+            volume.setValue(mp.getVolume()*100);
+            volume.valueProperty().addListener(new InvalidationListener() {
+                @Override
+                public void invalidated(Observable observable) {
+                    mp.setVolume(volume.getValue()/100);
+                }
+            });
+            volume.setStyle("-fx-control-inner-background: red;");
+        }
+    }
 
+
+
+
+    public void initialize() {
 
     }
-    public void play(){
+
+
+
+
+
+
+
+    public void play() {
         mp.play();
+        mp.setRate(1);
+
+    }
+    public void pause(){
+        mp.pause();
+    }
+    public void stop(){
+        mp.stop();
+    }
+    public void fast(){
+        mp.setRate(2);
+    }
+    public void slow(){
+        mp.setRate(.2);
     }
 
 
-    }
+}
 
 
 
